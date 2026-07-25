de5e6d4fd13bcbb57dc4c7f64e907fae0616f1b4
Mihir3102 <cs24f016@ybit.ac.in>
2026-07-25 16:35:55 +0530

    Add project folder

--- script.js
+++ script.js
@@ -6,16 +6,24 @@ function calci(op) {
     switch (op) {
         case "+":
             result = num1 + num2;
+            document.getElementById("result").innerHTML = "result:" + result;
             break;
         case "-":
             result = num1 - num2;
+            document.getElementById("result").innerHTML = "result:" + result;
             break;
         case "*":
             result = num1 * num2;
+            document.getElementById("result").innerHTML = "result:" + result;
             break;
         case "/":
-            result = num1 / num2;
-            break;
+            if (num2 == 0) {
+                document.getElementById("result").innerHTML = "Cannot divide by zero";
+            }
+            else {
+                result = num1 / num2;
+                document.getElementById("result").innerHTML = "result:" + result;
+                break;
+            }
     }
-    document.getElementById("result").innerHTML = "result:" + result;
 }
--- script.ts
+++ script.ts
@@ -11,13 +11,24 @@ function calci(op:string):void{
             switch (op)
              {
                 case "+": result=num1+num2;
+                    document.getElementById("result")!.innerHTML ="result:"+result;
                 break;
                  case "-": result=num1-num2;
+                     document.getElementById("result")!.innerHTML ="result:"+result;
+
                  break;
                   case "*": result=num1*num2;
+                      document.getElementById("result")!.innerHTML ="result:"+result;
                   break;   
-                   case "/": result=num1/num2;
-                   break;
+                   case "/": 
+                   if(num2==0) 
+                   {
+                    document.getElementById("result")!.innerHTML ="Cannot divide by zero";
+                   }
+                   else{
+                   result=num1/num2;
+                       document.getElementById("result")!.innerHTML ="result:"+result;
+                   break;}
             }
-    document.getElementById("result")!.innerHTML ="result:"+result;
+
 }
